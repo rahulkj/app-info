fd7e24c123b5f0494b3869398e8c9b4bc80a4996
Rahul Jain <rahulkj@gmail.com>
2024-08-26 16:49:45 -0500

    updating build script to invoke v2

--- ci/build.sh
+++ ci/build.sh
@@ -9,8 +9,8 @@ fi
 
 mkdir releases
 
-GOOS=linux GOARCH=amd64 go build -o releases/app-info-linux-amd64 github.com/rahulkj/app-info
+GOOS=linux GOARCH=amd64 go build -o releases/app-info-linux-amd64 github.com/rahulkj/app-info/v2
 
-GOOS=darwin GOARCH=amd64 go build -o releases/app-info-darwin-amd64 github.com/rahulkj/app-info
+GOOS=darwin GOARCH=amd64 go build -o releases/app-info-darwin-amd64 github.com/rahulkj/app-info/v2
 
-GOOS=windows GOARCH=386 go build -o releases/app-info-windows-amd64.exe github.com/rahulkj/app-info
+GOOS=windows GOARCH=386 go build -o releases/app-info-windows-amd64.exe github.com/rahulkj/app-info/v2
